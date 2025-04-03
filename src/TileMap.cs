87444615fb8752788f7a5332efc456810d812609
Alexander Poschmann <a.poschmann@nodapo.de>
2025-04-03 17:06:58 +0200

    Basic tile map terrain generatio (#4)
    * Add tile map and layers
    
    * Add terrain tiles, setup tile map
    
    * Generate basic tile map

---
+++ src/TileMap.cs
@@ -0,0 +1,33 @@
+using Godot;
+
+namespace de.nodapo.turnbasedstrategygame;
+
+public partial class TileMap : Node2D
+{
+    [Export] public int Width = 100;
+    [Export] public int Height = 60;
+
+    private TileMapLayer _baseLayer = null!;
+    private TileMapLayer _borderLayer = null!;
+    private TileMapLayer _overlayLayer = null!;
+
+    public override void _Ready()
+    {
+        _baseLayer = GetNode<TileMapLayer>("BaseLayer");
+        _borderLayer = GetNode<TileMapLayer>("BorderLayer");
+        _overlayLayer = GetNode<TileMapLayer>("OverlayLayer");
+
+        GenerateTerrain();
+    }
+
+    public void GenerateTerrain()
+    {
+        for (var x = 0; x < Width; x++)
+        {
+            for (var y = 0; y < Height; y++)
+            {
+                _baseLayer.SetCell(new Vector2I(x, y), 0, new Vector2I(0, 0));
+            }
+        }
+    }
+}
