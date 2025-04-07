8f2905500797776f09e6a502a8cd2e54154e2a89
Alexander Poschmann <a.poschmann@nodapo.de>
2025-04-07 16:04:16 +0200

    Improve nullable reference handling

--- src/TileMap.cs
+++ src/TileMap.cs
@@ -1,4 +1,3 @@
-using System;
 using Godot;
 
 namespace de.nodapo.turnbasedstrategygame;
@@ -12,23 +11,25 @@ public partial class TileMap : Node2D
     private TileMapLayer? _borderLayer;
     private TileMapLayer? _overlayLayer;
 
+    private TileMapLayer BaseLayer => _baseLayer ??= GetNode<TileMapLayer>("BaseLayer");
+    private TileMapLayer BorderLayer => _borderLayer ??= GetNode<TileMapLayer>("BorderLayer");
+    private TileMapLayer OverlayLayer => _overlayLayer ??= GetNode<TileMapLayer>("OverlayLayer");
+
     public override void _Ready()
     {
-        _baseLayer = GetNode<TileMapLayer>("BaseLayer") ?? throw new NullReferenceException();
-        _borderLayer = GetNode<TileMapLayer>("BorderLayer") ?? throw new NullReferenceException();
-        _overlayLayer = GetNode<TileMapLayer>("OverlayLayer") ?? throw new NullReferenceException();
-
         GenerateTerrain();
     }
 
-    public void GenerateTerrain()
+    public Vector2 ToLocal(Vector2I coordinates) => BaseLayer.MapToLocal(coordinates);
+
+    private void GenerateTerrain()
     {
         for (var x = 0; x < Width; x++)
         {
             for (var y = 0; y < Height; y++)
             {
-                _baseLayer?.SetCell(new Vector2I(x, y), 0, new Vector2I(0, 0));
-                _borderLayer?.SetCell(new Vector2I(x, y), 0, new Vector2I(0, 0));
+                BaseLayer.SetCell(new Vector2I(x, y), 0, new Vector2I(0, 0));
+                BorderLayer.SetCell(new Vector2I(x, y), 0, new Vector2I(0, 0));
             }
         }
     }
