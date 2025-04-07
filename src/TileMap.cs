52eb51df8f8d55be69305d7a4d46d5c2ab7cda27
Alexander Poschmann <a.poschmann@nodapo.de>
2025-04-07 15:39:13 +0200

    Add nullability

--- src/TileMap.cs
+++ src/TileMap.cs
@@ -1,3 +1,4 @@
+using System;
 using Godot;
 
 namespace de.nodapo.turnbasedstrategygame;
@@ -7,15 +8,15 @@ public partial class TileMap : Node2D
     [Export] public int Width = 100;
     [Export] public int Height = 60;
 
-    private TileMapLayer _baseLayer = null!;
-    private TileMapLayer _borderLayer = null!;
-    private TileMapLayer _overlayLayer = null!;
+    private TileMapLayer? _baseLayer;
+    private TileMapLayer? _borderLayer;
+    private TileMapLayer? _overlayLayer;
 
     public override void _Ready()
     {
-        _baseLayer = GetNode<TileMapLayer>("BaseLayer");
-        _borderLayer = GetNode<TileMapLayer>("BorderLayer");
-        _overlayLayer = GetNode<TileMapLayer>("OverlayLayer");
+        _baseLayer = GetNode<TileMapLayer>("BaseLayer") ?? throw new NullReferenceException();
+        _borderLayer = GetNode<TileMapLayer>("BorderLayer") ?? throw new NullReferenceException();
+        _overlayLayer = GetNode<TileMapLayer>("OverlayLayer") ?? throw new NullReferenceException();
 
         GenerateTerrain();
     }
@@ -26,8 +27,8 @@ public partial class TileMap : Node2D
         {
             for (var y = 0; y < Height; y++)
             {
-                _baseLayer.SetCell(new Vector2I(x, y), 0, new Vector2I(0, 0));
-                _borderLayer.SetCell(new Vector2I(x, y), 0, new Vector2I(0, 0));
+                _baseLayer?.SetCell(new Vector2I(x, y), 0, new Vector2I(0, 0));
+                _borderLayer?.SetCell(new Vector2I(x, y), 0, new Vector2I(0, 0));
             }
         }
     }
