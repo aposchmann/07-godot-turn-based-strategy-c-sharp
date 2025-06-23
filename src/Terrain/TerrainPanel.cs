2ef57121559196115f00c22888d4a506dd8b4a92
Alexander Poschmann <a.poschmann@nodapo.de>
2025-06-23 21:54:43 +0200

    Add terrain images

--- src/Terrain/TerrainPanel.cs
+++ src/Terrain/TerrainPanel.cs
@@ -1,11 +1,26 @@
 using System;
+using System.Collections.Generic;
 using de.nodapo.turnbasedstrategygame.Map;
 using Godot;
+using static de.nodapo.turnbasedstrategygame.Terrain.Terrain;
+using static Godot.ResourceLoader;
 
 namespace de.nodapo.turnbasedstrategygame.Terrain;
 
 public partial class TerrainPanel : Panel
 {
+    private static Dictionary<Terrain, Texture2D> TerrainTextures { get; } = new()
+    {
+        { Plains, Load<Texture2D>("res://textures/terrain/plains.jpg") },
+        { Beach, Load<Texture2D>("res://textures/terrain/beach.jpg") },
+        { Desert, Load<Texture2D>("res://textures/terrain/desert.jpg") },
+        { Mountain, Load<Texture2D>("res://textures/terrain/mountain.jpg") },
+        { Ice, Load<Texture2D>("res://textures/terrain/ice.jpg") },
+        { Water, Load<Texture2D>("res://textures/terrain/water.jpg") },
+        { Coast, Load<Texture2D>("res://textures/terrain/coast.jpg") },
+        { Forest, Load<Texture2D>("res://textures/terrain/forest.jpg") },
+    };
+
     private Label? _foodLabel;
     private Label? _productionLabel;
 
@@ -30,5 +45,7 @@ public partial class TerrainPanel : Panel
         TerrainLabel.Text = $"Terrain: {hex.Terrain}";
         FoodLabel.Text = $"Food: {hex.Food}";
         ProductionLabel.Text = $"Production: {hex.Production}";
+
+        TerrainImage.Texture = TerrainTextures[hex.Terrain];
     }
 }
