e52c3baadf0b6560a078dba9f32b272a19dbd4c9
Alexander Poschmann <a.poschmann@nodapo.de>
2025-07-31 17:44:09 +0200

    Unit build buttons

---
+++ src/unit/UnitBuildButton.cs
@@ -0,0 +1,16 @@
+using Godot;
+
+namespace de.nodapo.turnbasedstrategygame.unit;
+
+public partial class UnitBuildButton : Button
+{
+    [Signal]
+    public delegate void BuildUnitEventHandler(Unit unit);
+
+    public required Unit Unit { get; set; }
+
+    public override void _Ready()
+    {
+        Pressed += () => EmitSignalBuildUnit(Unit);
+    }
+}
